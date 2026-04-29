a63fb6b7f61f99676be7fc9009e97b8c33d08bb2
Nina-Qiu <cq2181@nyu.edu>
2026-04-29 13:10:06 +0800

    final project

--- D-final-project/script(marriage).js
+++ D-final-project/script(marriage).js
@@ -15,8 +15,37 @@ function windowWasScrolled() {
     let percentage = getScrollPercentage();
     console.log("percentage", percentage);
 
+    if (percentage > 0.3) {
+        document.querySelector(".intro p:nth-child(1)").style.transform = "translate(-500px, -500px) rotate(-45deg)";
+        document.querySelector(".cta").style.transform = "translateY(-100px)";
+        document.querySelector(".cta").style.opacity = "0";
+        document.querySelector(".arrow1").style.transform = "translateY(-100px)";
+        document.querySelector(".arrow1").style.opacity = "0";
+    } else {
+        document.querySelector(".intro p:nth-child(1)").style.transform = "translate(0px, 0px) rotate(0deg)";
+        document.querySelector(".cta").style.transform = "translateY(0px)";
+        document.querySelector(".cta").style.opacity = "1";
+        document.querySelector(".arrow1").style.transform = "translateY(0px)";
+        document.querySelector(".arrow1").style.opacity = "1";
+    }
 
+    if (percentage > 0.32) {
+        document.querySelector(".intro p:nth-child(2)").style.transform = "translate(-500px, -500px) rotate(-45deg)";
+    } else {
+        document.querySelector(".intro p:nth-child(2)").style.transform = "translate(0px, 0px) rotate(0deg)";
+    }
 
+    if (percentage > 0.34) {
+        document.querySelector(".intro p:nth-child(3)").style.transform = "translate(-500px, -500px) rotate(-45deg)";
+    } else {
+        document.querySelector(".intro p:nth-child(3)").style.transform = "translate(0px, 0px) rotate(0deg)";
+    }
+
+    if (percentage > 0.36) {
+        document.querySelector(".intro p:nth-child(4)").style.transform = "translate(-500px, -500px) rotate(-45deg)";
+    } else {
+        document.querySelector(".intro p:nth-child(4)").style.transform = "translate(0px, 0px) rotate(0deg)";
+    }
 
     // this section happens from 0 - 20 percent
     if (percentage < 20) {
@@ -41,7 +70,6 @@ function windowWasScrolled() {
             document.querySelector(".house").style.transform = "translateX(600px)";
             document.querySelector(".paper").style.transform = "translateX(1000px)";
             document.querySelector(".billWrapper").style.transform = "translateX(600px)";
-
         } else {
             document.querySelector(".flower1").style.transform = "translateX(0px)";
             document.querySelector(".flower2").style.transform = "translateX(0px) rotate(30deg)";
@@ -70,10 +98,14 @@ function windowWasScrolled() {
 
     if (percentage > 37){
             document.querySelector(".people2").style.transform = "translateX(-400px)";
+    } else {
+            document.querySelector(".people2").style.transform = "translateX(0px)";
     }
 
     if(percentage > 39){
         document.querySelector(".gossip").style.transform = "translateX(-550px)";
+    } else {
+        document.querySelector(".gossip").style.transform = "translateX(0px)";
     }
 }
 
